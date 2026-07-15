a165c33ab2abfcc0e31d8d007a039163ef703274
Wang Han <416810799@qq.com>
2026-07-16 00:21:20 +0800

    Disable verification agent by passing INSTALL_DISABLE_VERIFICATION
    isVerificationEnabled is inlined in OneUI.

--- app/src/main/java/org/lsposed/corepatch/hook/VerifyingSessionHook.kt
+++ app/src/main/java/org/lsposed/corepatch/hook/VerifyingSessionHook.kt
@@ -9,6 +9,8 @@ import org.lsposed.corepatch.XposedHelper.hostClassLoader
 object VerifyingSessionHook : BaseHook() {
     override val name = "VerifyingSessionHook"
 
+    private const val INSTALL_DISABLE_VERIFICATION = 0x00080000
+
     @SuppressLint("PrivateApi")
     override fun hook() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
@@ -18,13 +20,30 @@ object VerifyingSessionHook : BaseHook() {
         val verifyingSessionClazz =
             hostClassLoader.loadClass("com.android.server.pm.VerifyingSession")
 
-        val isVerificationEnabledMethod =
-            verifyingSessionClazz.declaredMethods.first { m -> m.name == "isVerificationEnabled" }
-        hookBefore(isVerificationEnabledMethod) { callback ->
+        val installFlagsField =
+            verifyingSessionClazz.getDeclaredField("mInstallFlags").apply { isAccessible = true }
+        val handleStartVerifyMethod =
+            verifyingSessionClazz.declaredMethods.first { m -> m.name == "handleStartVerify" }
+        hookBefore(handleStartVerifyMethod) { callback ->
+            if (Config.isDisableVerificationAgentEnabled()) {
+                val session = callback.thisObject ?: return@hookBefore
+                installFlagsField.setInt(
+                    session,
+                    installFlagsField.getInt(session) or INSTALL_DISABLE_VERIFICATION
+                )
+            }
+        }
+
+        val isAdbVerificationEnabledMethod =
+            verifyingSessionClazz.declaredMethods.first { m ->
+                m.name == "isAdbVerificationEnabled" &&
+                    m.returnType == Boolean::class.java
+            }
+
+        hookBefore(isAdbVerificationEnabledMethod) { callback ->
             if (Config.isDisableVerificationAgentEnabled()) {
                 callback.returnAndSkip(false)
             }
         }
-
     }
 }
